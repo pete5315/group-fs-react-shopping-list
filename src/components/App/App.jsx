58fc73f6254eb0b5176a95826e3a7dd60996da49
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-01 14:49:01 -0500

    fixed axios error

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import axios from axios;
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 import Item from '../Item/Item.jsx';
 import ShoppingList from '../ShoppingList/shoppingList.jsx';
 import ItemForm from '../ItemForm/ItemForm.jsx';
+import {useState} from 'react';
 
 function App() {
 
