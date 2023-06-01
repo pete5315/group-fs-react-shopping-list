26c18f06972dfb5279cda0f5af3e31025d844f76
Mahamud Aden <mahamudadn@gmail.com>
2023-06-01 13:55:42 -0500

    added indented

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,38 +1,38 @@
-function Item() {
+    function Item() {
 
-    return (
-        <>
-        <div>
-            <h4>Apples</h4>
-            <p>5 lbs</p>
+        return (
+            <>
+            <div>
+                <h4>Apples</h4>
+                <p>5 lbs</p>
+                <button>Buy</button>
+                <button>Remove</button>
+            </div>
+            <div>
+            <h4>Bread</h4>
+            <p>5 loof</p>
             <button>Buy</button>
             <button>Remove</button>
         </div>
-         <div>
-         <h4>Bread</h4>
-         <p>5 loof</p>
-         <button>Buy</button>
-         <button>Remove</button>
-     </div>
-     <div>
-         <h4>Milk</h4>
-         <p> 1 galloon</p>
-         <button>Buy</button>
-         <button>Remove</button>
-     </div>
-     <div>
-         <h4>Sliced Almonds</h4>
-         <p>2 cups</p>
-         <button>Buy</button>
-         <button>Remove</button>
-     </div>
-     <div>
-         <h4>Bananas</h4>
-         <p> 1 bunch</p>
-         <button>Buy</button>
-         <button>Remove</button>
-     </div>
-     </>
-    )
-    
-}
+        <div>
+            <h4>Milk</h4>
+            <p> 1 galloon</p>
+            <button>Buy</button>
+            <button>Remove</button>
+        </div>
+        <div>
+            <h4>Sliced Almonds</h4>
+            <p>2 cups</p>
+            <button>Buy</button>
+            <button>Remove</button>
+        </div>
+        <div>
+            <h4>Bananas</h4>
+            <p> 1 bunch</p>
+            <button>Buy</button>
+            <button>Remove</button>
+        </div>
+        </>
+        )
+        
+    }
