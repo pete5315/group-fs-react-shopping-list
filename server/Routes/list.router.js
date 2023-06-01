0f1df1dfd059615c70bd937dd08cb2f658184024
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-01 15:20:40 -0500

    added pull route on the server side.

--- server/Routes/list.router.js
+++ server/Routes/list.router.js
@@ -9,13 +9,32 @@ router.delete('/:id', (req, res) => {
   // Let sql sanitize your inputs (NO Bobby Drop Tables here!)
   // the $1, $2, etc get substituted with the values from the array below
   pool.query(sqlText, [itemID])
-      .then((result) => {
-          res.sendStatus(201);
-      })
-      .catch((error) => {
-          console.log(`Error making database query ${sqlText}`, error);
-          res.sendStatus(500); // Good server always responds
-      })
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+  })
+})
+
+
+router.put('/:id', (req, res) => {
+  const itemID = req.params.id;
+  const itemIsPurchased=req.body.purchased;
+  console.log (itemID, itemIsPurchased);
+  const sqlText = `UPDATE "list" SET "purchased" = TRUE WHERE id=$2;`;
+  pool.query(sqlText, [itemIsPurchased,itemID])
+      //if SQL doesn't error
+  .then( (result) => {
+    console.log('Task completed')
+    res.sendStatus(200);
+  })
+      //if SQL DOES error
+  .catch( (error) => {
+    console.log(`Error making database query ${sqlText}`, error);
+    res.sendStatus(500);
+  })
 })
 
 
