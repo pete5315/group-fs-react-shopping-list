2d486e7942f63339d6768f4a34b6475f38cfd398
Mahamud Aden <mahamudadn@gmail.com>
2023-06-01 11:51:25 -0500

    added Shopping List component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
+import ShoppingList from '../ShoppingList/shoppingList.jsx';
 
 
 function App() {
     return (
         <div className="App">
             <Header />
+
             <main>
+                <ShoppingList/>
                 <p>Under Construction...</p>
             </main>
         </div>
---
+++ src/components/ShoppingList/shoppingList.jsx
@@ -0,0 +1,10 @@
+
+function ShoppingList() {
+    return (
+        <h2>Shopping List</h2>
+    )
+    
+}
+
+
+export default ShoppingList;
