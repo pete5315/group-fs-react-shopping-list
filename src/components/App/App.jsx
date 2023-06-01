f531c21bff95b2012b529a4bba08812e8c2b25e5
Mahamud Aden <mahamudadn@gmail.com>
2023-06-01 16:18:46 -0500

    added clint side put

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,53 +1,72 @@
-import React from 'react';
-import axios from 'axios';
-import Header from '../Header/Header.jsx'
-import './App.css';
-import Item from '../Item/Item.jsx';
-import ShoppingList from '../ShoppingList/shoppingList.jsx';
-import ItemForm from '../ItemForm/ItemForm.jsx';
-import {useState} from 'react';
+    import React from 'react';
+    import axios from 'axios';
+    import Header from '../Header/Header.jsx'
+    import './App.css';
+    import Item from '../Item/Item.jsx';
+    import ShoppingList from '../ShoppingList/shoppingList.jsx';
+    import ItemForm from '../ItemForm/ItemForm.jsx';
+    import {useState} from 'react';
+    import pool from '../../../server/modules/pool.js';
 
-function App() {
-
-
-  let [list, setList] = useState([]);
+    function App() {
 
 
-
-  const getList = () => {
-    axios.get('/list')
-      .then(response => {
-        setList(response.data)
-      })
-      .catch(err => {
-        alert('error getting guests');
-        console.log(err);
-      })
-  }
+    let [list, setList] = useState([]);
 
 
 
+    const getList = () => {
+        axios.get('/list')
+        .then(response => {
+            setList(response.data)
+        })
+        .catch(err => {
+            alert('error getting list');
+            console.log(err);
+        })
+    }
 
-    return (
-        <div className="App">
-            <Header />
+    const updateList = (id, data) => {
+        axios.put('/list'+id, data)
 
-            <main>
+        
+        .then(response => {
+            getList()
+        })
+        .catch(err => {
+            alert('error getting list');
+            console.log(err);
+        })
 
-            
-                    <p>Under Construction...</p>
 
-                <ItemForm />
-      
-                <ShoppingList getList={getList} />
+    }
+
+
+
+
+
+
+
+        return (
+            <div className="App">
+                <Header />
+
+                <main>
+
                 
-                <Item/>
-            
+                        <p>Under Construction...</p>
 
-               
-            </main>
-        </div>
-    );
-}
+                    <ItemForm />
+        
+                    <ShoppingList getList={getList} />
+                    
+                    <Item/>
+                
 
-export default App;
+                
+                </main>
+            </div>
+        );
+    }
+
+    export default App;
