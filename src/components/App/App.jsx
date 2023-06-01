25786620bf56151f99354729b766661c5dba51dc
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-01 12:30:37 -0500

    set up axios get

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from axios;
 
 import Header from '../Header/Header.jsx'
 import './App.css';
@@ -6,12 +7,32 @@ import ShoppingList from '../ShoppingList/shoppingList.jsx';
 
 
 function App() {
+
+
+  let [list, setList] = useState([]);
+
+
+
+  const getList = () => {
+    axios.get('/list')
+      .then(response => {
+        setList(response.data)
+      })
+      .catch(err => {
+        alert('error getting guests');
+        console.log(err);
+      })
+  }
+
+
+
+
     return (
         <div className="App">
             <Header />
 
             <main>
-                <ShoppingList/>
+                <ShoppingList getList={getList} />
                 <p>Under Construction...</p>
             </main>
         </div>
